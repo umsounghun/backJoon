ce8b98fce5241bc979e6d8755be99eef84fa9236
Sounghun Um <korea970112@naver.com>
2022-06-16 11:52:16 +0900

    백준 빼기, 나누기, 곱하기

---
+++ src/main/java/com/example/backjun/beakjoon/Print/B1008_A나누기B.java
@@ -0,0 +1,23 @@
+package com.example.backjun.beakjoon.Print;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 참고 블로그 : https://st-lab.tistory.com/12
+public class B1008_A나누기B {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        double a = Integer.parseInt(st.nextToken());
+        double b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a / b);
+    }
+
+
+}
