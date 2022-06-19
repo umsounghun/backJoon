dea007e2f49672d2d4ef8496213961fb64500667
Sounghun Um <korea970112@naver.com>
2022-06-19 21:11:51 +0900

    doc - 백준 문제

---
+++ src/main/java/com/example/backjun/beakjoon/Print/B25083_새싹.java
@@ -0,0 +1,21 @@
+package com.example.backjun.beakjoon.Print;
+
+import java.io.IOException;
+
+public class B25083_새싹 {
+    public static void main(String[] args) throws IOException {
+
+        String A = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+
+        System.out.println(A);
+
+
+    }
+
+
+}
