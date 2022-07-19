a2d22cc21449d0e43bcd342b2df65eb17b05f8cd
Sounghun Um <korea970112@naver.com>
2022-07-19 21:57:43 +0900

    doc - 백준 문제

---
+++ src/main/java/com/example/backjun/beakjoon/B10773_제로.java
@@ -0,0 +1,41 @@
+package com.example.backjun.beakjoon;
+
+import java.util.Stack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class B10773_제로 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int K = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < K; i++) {
+            int number = Integer.parseInt(br.readLine());	// 정수 입력
+
+            if(number == 0) {	// 0이라면 스택에 저장된 top 원소를 지운다.
+                stack.pop();
+            }
+            else {
+                /*
+                 * push() 대신 add()로 대체해도 됨 (똑같이 상단에 원소를 추가하는 메소드다.)
+                 * ex) stack.add(number);
+                 */
+                stack.push(number);
+            }
+        }
+        int sum = 0;
+
+        for(int o : stack) {
+            sum += o;
+        }
+
+        System.out.println(sum);
+    }
+
+}
